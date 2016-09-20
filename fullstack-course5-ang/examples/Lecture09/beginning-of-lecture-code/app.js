2f67b2a19e7e901c8cbf9c7d7451967196b8e212
Mina Chuong <mina.chuong@gmail.com>
2016-09-20 16:18:23 -0700

    Lecture 9 comments

--- fullstack-course5-ang/examples/Lecture09/beginning-of-lecture-code/app.js
+++ fullstack-course5-ang/examples/Lecture09/beginning-of-lecture-code/app.js
@@ -2,10 +2,31 @@
 'use strict';
 
 angular.module('DIApp', [])
-.controller('DIController', DIController);
+  .controller('DIController', DIController);
 
-function DIController ($scope) {
-  $scope.name = "Yaakov";
-}
+  function DIController ($scope, $filter) {
+    $scope.name = "Yaakov";
+    // $filter service transforms data
+    $scope.upper = function () {
+      var upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
+    };
+  }
+
+  function AnnotateMe(name, job, something) {
+    return ":)";
+  }
+
+  // returns whatever the function returns
+  console.log(AnnotateMe());
+
+  // returns the function object
+  console.log(AnnotateMe);
+
+  // returns the function object as a string
+  console.log(AnnotateMe.toString());
 
 })();
+
+
+// KEY CONCEPT: angular uses regex to figure out dependencies
